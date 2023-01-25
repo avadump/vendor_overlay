5b8584cbcb7507fedf37449020e7878a98391472
minaripenguin37 <alexfinhart@gmail.com>
2023-01-26 04:22:40 +0545

    overlay: Add riceball icon shape
    Signed-off-by: Eidoron1 <eidoronp67@gmail.com>

--- overlays.mk
+++ overlays.mk
@@ -63,6 +63,7 @@ PRODUCT_PACKAGES += \
     IconShapeCylindricalOverlay \
     IconShapeStretchedOverlay \
     IconShapePebbleOverlay \
+    IconShapeRiceBallsOverlay \
     IconShapeRoundedRectOverlay \
     IconShapeSquareOverlay \
     IconShapeSquircleOverlay \
